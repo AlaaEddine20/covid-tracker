1ebb482047eece260384c834ffd48ee721c2713a
Alaa Eddine <alaneddin91@gmail.com>
2021-04-07 22:57:26 +0200

    adds fetch data api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React from "react";
-import "./App.css";
+import React, { useEffect, useState } from "react";
+import { fetchData } from "./api/index";
+import styles from "./App.module.scss";
 
 function App() {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    async function dataFetch() {
+      const fetchedData = await fetchData();
+      setData(fetchedData);
+    }
+    dataFetch();
+  }, []);
+
   return (
-    <div className="App">
+    <div className={styles.container}>
       <h1>App</h1>
     </div>
   );
