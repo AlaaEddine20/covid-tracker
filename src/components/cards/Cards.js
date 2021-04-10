b8125fcabbab54bc6a1df0a0847ec4e707ef5965
Alaa Eddine <mondo@live.it>
2021-04-10 16:47:10 +0200

    adding material-ui styles

--- src/components/cards/Cards.js
+++ src/components/cards/Cards.js
@@ -1,33 +1,34 @@
 import React from "react";
+import Paper from "@material-ui/core/Paper";
 import styles from "./Styles.module.scss";
 
 const Cards = ({ confirmed, recovered, deaths, lastUpdate }) => {
   return (
     <div className={styles.cards_container}>
-      <div className={styles.confirmed_card}>
+      <Paper className={styles.confirmed_card} elevation={3}>
         <h2>Confirmed</h2>
         {confirmed ? (
           <p>{confirmed.value.toLocaleString("en")}</p>
         ) : (
-          <p>Loading..."</p>
+          <p>Loading...</p>
         )}
-      </div>
-      <div className={styles.recovered_card}>
+      </Paper>
+      <Paper className={styles.confirmed_card} elevation={4}>
         <h2>Recovered</h2>
         {recovered ? (
           <p>{recovered.value.toLocaleString("en")}</p>
         ) : (
-          <p>Loading..."</p>
+          <p>Loading...</p>
         )}
-      </div>
-      <div className={styles.deaths_card}>
+      </Paper>
+      <Paper className={styles.confirmed_card} elevation={4}>
         <h2>Deaths</h2>
         {deaths ? (
           <p>{deaths.value.toLocaleString("en")}</p>
         ) : (
-          <p>Loading..."</p>
+          <p>Loading...</p>
         )}
-      </div>
+      </Paper>
     </div>
   );
 };
--- src/components/charts/Charts.js
+++ src/components/charts/Charts.js
@@ -1,11 +1,12 @@
+import { Paper } from "@material-ui/core";
 import React from "react";
 import styles from "./Styles.module.scss";
 
 const Charts = () => {
   return (
-    <div className={styles.charts_container}>
+    <Paper elevation={3} className={styles.charts_container}>
       <h2>Charts</h2>
-    </div>
+    </Paper>
   );
 };
 
