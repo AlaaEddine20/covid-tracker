dff16e0440c336c0a22eb3c5c4ff643ef00e81d9
Alaa Eddine <mondo@live.it>
2021-04-10 12:01:12 +0200

    dispaying fetched data in ui

--- src/components/cards/Cards.js
+++ src/components/cards/Cards.js
@@ -1,17 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Styles.module.scss";
 
-const Cards = ({ data }) => {
+const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
   return (
     <div className={styles.cards_container}>
       <div>
-        <h1>Card 1</h1>
+        <h1>Confirmed</h1>
+        {confirmed.value ? (
+          <p>{confirmed.value.toLocaleString("en")}</p>
+        ) : (
+          <p>Loading..."</p>
+        )}
       </div>
       <div>
-        <h1>Card 2</h1>
+        <h1>Recovered</h1>
+        {recovered.value ? (
+          <p>{recovered.value.toLocaleString("en")}</p>
+        ) : (
+          <p>Loading..."</p>
+        )}
       </div>
       <div>
-        <h1>Card 3</h1>
+        <h1>Deaths</h1>
+        {deaths.value ? (
+          <p>{deaths.value.toLocaleString("en")}</p>
+        ) : (
+          <p>Loading..."</p>
+        )}
       </div>
     </div>
   );
