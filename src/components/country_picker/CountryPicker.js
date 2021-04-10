63e3d83980a533eb563eafc59f6385b993c29291
Alaa Eddine <mondo@live.it>
2021-04-10 16:22:22 +0200

    adding styles to cards

--- src/components/country_picker/CountryPicker.js
+++ src/components/country_picker/CountryPicker.js
@@ -1,12 +1,65 @@
 import React from "react";
+// material ui
+import { makeStyles } from "@material-ui/core/styles";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+// styles
 import styles from "./Styles.module.scss";
 
 const CountryPicker = () => {
+  const classes = useStyles();
+  const [country, setCountry] = React.useState("");
+  const [open, setOpen] = React.useState(false);
+
+  const handleChange = (e) => {
+    setCountry(e.target.value);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
   return (
     <div className={styles.picker_container}>
-      <h1>Country picker</h1>
+      <FormControl className={classes.formControl}>
+        <InputLabel id="demo-controlled-open-select-label">Country</InputLabel>
+        <Select
+          labelId="demo-controlled-open-select-label"
+          id="demo-controlled-open-select"
+          open={open}
+          onClose={handleClose}
+          onOpen={handleOpen}
+          value={country}
+          onChange={handleChange}
+        >
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </FormControl>
     </div>
   );
 };
+
+const useStyles = makeStyles((theme) => ({
+  button: {
+    display: "block",
+    marginTop: theme.spacing(2),
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 150,
+    display: "flex",
+  },
+}));
 
 export default CountryPicker;
